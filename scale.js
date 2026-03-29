2bab5072bb706f3e537dbc110b712276df2eb410
1anRobinson <alex.robi.2020@gmail.com>
2026-03-29 06:16:38 -0700

    wow. that was so incredibly easy jesus

--- scale.js
+++ scale.js
@@ -1,22 +1,11 @@
-let scale = 1;
-const el = document.querySelector("#ball");
+const elem = document.getElementById("ball")
+const panzoom = Panzoom(elem, {
+  maxScale: 50
+})
+panzoom.pan(10, 10)
+panzoom.zoom(2, { animate: true })
 
-function zoom(event) {
-
-    event.preventDefault();
-    scale += event.deltaY * -0.01;
-    // Restrict scale
-    scale = Math.min(Math.max(0.125, scale), 4);
-
-    // Apply scale transform
-    el.style.transform = `scale(${scale})`;
-}
-
-function pan(event) {
-    event.preventDefault();
-    el.style.cursor = "alias";
-    el.style.color = "green";
-}
-
-el.onwheel = zoom;
-el.onclick = pan;
+// Panning and pinch zooming are bound automatically (unless disablePan is true).
+// There are several available methods for zooming
+// that can be bound on button clicks or mousewheel.
+elem.parentElement.addEventListener('wheel', panzoom.zoomWithWheel)
