b1a155681dbd15f5aedc56489add564b3da8049a
1anRobinson <alex.robi.2020@gmail.com>
2026-08-08 16:59:21 -0700

    moving on to implementing taxonomy map

--- scale.js
+++ scale.js
@@ -1,11 +1,15 @@
-const elem = document.getElementById("ball")
+const elem = document.getElementById("map")
 const panzoom = Panzoom(elem, {
-  maxScale: 50
+  maxScale: 100,
+  // minScale:10,
+  step: 0.15,
+  focal: {x: 0, y: 0}
 })
-panzoom.pan(10, 10)
-panzoom.zoom(2, { animate: true })
+// panzoom.pan(100, 100)
+panzoom.zoom(1, { animate: true })
 
 // Panning and pinch zooming are bound automatically (unless disablePan is true).
 // There are several available methods for zooming
 // that can be bound on button clicks or mousewheel.
 elem.parentElement.addEventListener('wheel', panzoom.zoomWithWheel)
+
